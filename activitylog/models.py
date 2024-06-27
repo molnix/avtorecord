04e5777b04524c8255542e287a293a0034769b61
molnix <molnixv0.2@gmail.com>
2024-06-27 17:40:52 +0700

    create models

--- activitylog/models.py
+++ activitylog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    all_mileage = models.FloatField(default=0.0, help_text='km.m')
+    busy = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    email = models.CharField(max_length=200, unique=True)
+    login = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200) 
+
+    def __str__(self):
+        return self.login
+
+
+class ActivityLog(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    get_time = models.DateTimeField(auto_now_add=True)
+    return_time = models.DateTimeField(blank=True)
+    travel_start_point = models.TextField(blank=True)
+    travel_target_point = models.TextField(blank=True)
+    travel_mileage = models.FloatField(default=0.0, help_text='km.m')
+
+    def __str__(self) -> str:
+        return super().__str__()
